d5854e69f335e6ead823dc07bbc5c4d4353a8df6
hobs <hobs+github@totalgood.com>
2019-06-30 19:23:36 -0700

    trec question classification dataset ingester

---
+++ src/nlpia/trec.py
@@ -0,0 +1,128 @@
+""" TREC Question Classification Dataset
+
+~5000 sentences classified into ~5 types of Questions
+
+Taxonomy:
+Class   Definition
+ABBREVIATION    abbreviation
+  abb   abbreviation
+  exp   expression abbreviated
+ENTITY  entities
+  animal    animals
+  body  organs of body
+  color     colors
+  creative  inventions, books and other creative pieces
+  currency  currency names
+  dis.med.  diseases and medicine
+  event     events
+  food  food
+  instrument    musical instrument
+  lang  languages
+  letter    letters like a-z
+  other     other entities
+  plant     plants
+  product   products
+  religion  religions
+  sport     sports
+  substance     elements and substances
+  symbol    symbols and signs
+  technique     techniques and methods
+  term  equivalent terms
+  vehicle   vehicles
+  word  words with a special property
+DESCRIPTION     description and abstract concepts
+  definition    definition of sth.
+  description   description of sth.
+  manner    manner of an action
+  reason    reasons
+HUMAN   human beings
+  group     a group or organization of persons
+  ind   an individual
+  title     title of a person
+  description   description of a person
+LOCATION    locations
+  city  cities
+  country   countries
+  mountain  mountains
+  other     other locations
+  state     states
+NUMERIC     numeric values
+  code  postcodes or other codes
+  count     number of sth.
+  date  dates
+  distance  linear measures
+  money     prices
+  order     ranks
+  other     other numbers
+  period    the lasting time of sth.
+  percent   fractions
+  speed     speed
+  temp  temperature
+  size  size, area and volume
+  weight    weight
+"""
+import os
+import regex
+import requests
+
+import pandas as pd
+
+
+# df = pd.concat([pd.read_html(f'http://cogcomp.org/Data/QA/QC/train_{i}.label')
+#                 for i in (1000, 2000, 3000, 4000, 5500)], columns=['line'])
+
+TREC_FILES = ['TREC_10.label'] + [f'train_{i}.label' for i in (1000, 2000, 3000, 4000, 5500)]
+TREC_URLS = tuple([f'http://cogcomp.org/Data/QA/QC/{fn}' for fn in TREC_FILES])
+TREC_CLASSES = [
+    ('ABBR', 'ABBREVIATION', 'abbreviation'),
+    ('ENTY', 'ENTITY', 'entities'),
+    ('DESC', 'DESCRIPTION', 'description and abstract concepts'),
+    ('HUM', 'HUMAN', 'human beings'),
+    ('LOC', 'LOCATION', 'locations'),
+    ('NUM', 'NUMERIC', 'numeric values'),
+    ]
+TREC_CLASS_ABBREVIATIONS = dict([(abb, name) for abb, name, desc in TREC_CLASSES])
+
+
+def download_trec_urls(urls=TREC_URLS, output_filepath='trec_dataset.csv'):
+    """ Download all 6 .label files for the TREC dataset and concatenate them into a DataFrame
+
+    >>> df = download_trec_urls()
+    >>> df.describe(include='all')
+    >>> df.head()
+      dataset        class subclass                                  sentence
+    0    test      NUMERIC     dist      How far is it from Denver to Aspen ?
+    1    test     LOCATION     city  What county is Modesto , California in ?
+    2    test        HUMAN     desc                         Who was Galileo ?
+    3    test  DESCRIPTION      def                         What is an atom ?
+    4    test      NUMERIC     date          When did Hawaii become a state ?
+           dataset   class subclass                 sentence
+    count     5882    5882     5882                     5882
+    unique       2       6       47                     5871
+    top      train  ENTITY      ind  What is the Milky Way ?
+    freq      5382    1339     1011                        2
+    """
+    responses = [requests.get(url) for url in TREC_URLS]
+    lines = [('train' if 'train_' in r.url else 'test') + ':' + line for r in responses for line in r.text.splitlines()]
+
+    lines = [tuple(regex.match(r'(train|test)[:]([A-Z]+)[:]([a-z]+)\s(.+)', line).groups()) for line in lines]
+    df = pd.DataFrame(lines, columns='dataset class subclass sentence'.split())
+    df = df.drop_duplicates()
+    # df.describe(include='all')
+    df['class'] = df['class'].apply(lambda x: TREC_CLASS_ABBREVIATIONS.get(x, x))
+    return df
+
+
+def read_trec_files(filepaths=TREC_FILES):
+    """ process files like train_1000.label if they've already been downloaded (e.g. using curl or wget) """
+    lines = []
+    for fn in os.listdir():
+        if fn.endswith('.label'):
+            with open(fn, 'rb') as fin:
+                lines.extend(('train', x) for x in fin.readlines())
+    lines.extend([('test', x) for x in open('http://cogcomp.org/Data/QA/QC/TREC_10.label', 'rb').readlines()])
+    lines = [(line[0], line[1].decode('latin')) for line in lines]
+    lines = [[line[0]] + list(regex.match(r'([A-Z]+)[:]([a-z]+)\s(.+)', line[1])) for line in lines]
+    # df = pd.concat([pd.read_html(f'http://cogcomp.org/Data/QA/QC/train_{i}.label')
+    #                 for i in (1000, 2000, 3000, 4000, 5500)], columns=['line'])
+    return pd.DataFrame(lines, columns='dataset class subclass sentence'.split())
